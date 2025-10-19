47a6a23656c9763871322bad9754cd21d8c69f4f
owlstechbd <owlstech.net@gmail.com>
2025-10-19 20:17:27 +0600

    dsfhhgds

--- js/main.js
+++ js/main.js
@@ -10,5 +10,19 @@ window.addEventListener('scroll', function() {
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      bsCollapse.hide();
+    });
+  });
+});
+
 //animated
 AOS.init();
